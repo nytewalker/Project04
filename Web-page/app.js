255dd0b85a931c6f7436a4b8f0f3bc0708a2d5cb
Azu <142156871+nytewalker@users.noreply.github.com>
2025-05-30 15:25:58 +0100

    Add files via upload
    worked webpage with scroll animation... initail used the observer but changed to plain css... still working on it dooh

---
+++ Web-page/app.js
@@ -0,0 +1,16 @@
+const hiddenElements = document.querySelectorAll('.hidden');
+
+
+const Observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+hiddenElements.forEach((el)=> Observer.observe(el));
